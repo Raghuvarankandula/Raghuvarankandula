ddd632ed8480e03a459bbbcdc4b89f9c8eab735a
Raghuvarankandula <110094270+Raghuvarankandula@users.noreply.github.com>
2022-09-09 19:59:43 +0530

    Create README.py

---
+++ README.py
@@ -0,0 +1,10 @@
+- 👋 Hi, I’m @Raghuvarankandula
+- 👀 I’m interested in coding
+- 🌱 I’m currently learning DataScience
+- 💞️ I’m looking to collaborate on Data Management
+- 📫 How to reach me kandularaghuvaran@gmail.com
+
+<!---
+Raghuvarankandula/Raghuvarankandula is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
